62f1b1dabeeea3acc029a73286d2f08c6b5ad20e
Kristín Fríða Sigurborgardóttir <143341930+KristinFrida@users.noreply.github.com>
2023-11-19 20:48:26 +0000

    Revert "changed a little conserning the comments"

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
 import { empty } from './lib/elements.js';
 import { renderFrontpage } from './lib/ui.js';
 
-async function onSearch(e) {
-    e.preventDefault();
-
-    if (!e.target || !(e.target instanceof Element)) {
-        return;
-    }
-
-    const { value } = e.target.querySelector('input') ?? {};
-
-    if (!value) {
-        return;
-    }
-
-    // This is an await, searchAndRender is waiting for it to finish displaying the result
-    // from this search. When it's done, it changes our URL and sets the query as the value,
-    // so now we have connected these together and are letting our entire program
-    // respond at specific points in different contexts.
-
-    await searchAndRender(document.body, e.target, value);
-    window.history.pushState({}, '', `/?query=${value}`);
-}
-
 /**
- * Checks which page we are on based on the query string and displays it.
- * If `id` is given, it is displayed, otherwise, the front page is displayed
- * with search results if `query` is given.
- */
+* Athugar hvaða síðu við erum á út frá query - string og birtir.
+* Ef`id` er gefið þá er það birt, annars er forsíða birt með
+* leitarniðurstöðum ef `query` er gefið.
+*/
 function route() {
     const { search } = window.location;
 
@@ -41,17 +19,16 @@ function route() {
     if (id) {
         renderDetails(parentElement, id);
     } else {
-        renderFrontpage(parentElement, onSearch, query);
+        renderFrontpage(parentElement, query);
     }
 }
-
-// Responds when using the browser to go back or forward.
+// Bregst við því þegar við notum vafra til að fara til baka eða áfram.
 window.onpopstate = () => {
-    /* TODO: Respond */
+    /* TODO bregðast við */
     empty(document.body);
 
     route();
 };
 
-// Check initially what should be displayed.
+// Athugum í byrjun hvað eigi að birta.
 route();
